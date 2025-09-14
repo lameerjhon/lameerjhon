969ef87d982b02ee83bf543551cbb047c40d04f5
lameerjhon <sbhatyisrael@gmail.com>
2025-09-14 16:26:21 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'Show Love',
+        path: 'music/Show Love.mp3',
+        artist: 'La &apos; meer Jhon',
+        cover: 'img/cover.png'
+    },
+    {
+        name: 'Why Don&apos;t The Birds Sing Anymore',
+        path: 'music/The Birds.mp3',
+        artist: 'La &apos; meer Jhon',
+        cover: 'img/cover.png'
+    }
+]
