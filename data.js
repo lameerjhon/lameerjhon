3ce722cf967333aa8472d05a513868b71d84585d
lameerjhon <sbhatyisrael@gmail.com>
2025-09-22 09:27:33 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Show Love',
+        path: 'music/Show Love.mp3',
+        artist: 'La &apos; meer Jhon',
+        cover: 'img/cover.png'
+    },
+    {
+        name: 'Why Don&apos;t The Birds Sing Anymore',
+        path: 'music/The Birds.mp3',
+        artist: 'La &apos; meer Jhon',
+        cover: 'img/cover.png'
+    },
+    {
+        name: 'It Ain&apos;t Over',
+        path: 'music/It Aint Over.mp3',
+        artist: 'La &apos; meer Jhon',
+        cover: 'img/itaintover.png'
+    }
+]
